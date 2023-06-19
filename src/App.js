0e2f07b6ca9328a534c61b2bda7dc65338a695c5
Twinkle126 <ty26yadav@gmail.com>
2023-06-19 23:55:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Recommend from "./Components/Recommend";
+import ScrollTop from "./Components/ScrollTop";
+import Service from "./Components/Service";
+import Testimonials from "./Components/Testimonials";
+import Footer from "./Components/Footer";
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScrollTop></ScrollTop>
+      <Navbar>Hi</Navbar>
+      <Hero></Hero>
+      <Service></Service>
+      <Recommend></Recommend>
+      <Testimonials></Testimonials>
+      <Footer></Footer>
     </div>
   );
 }
-
-export default App;
